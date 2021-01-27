afb6f5ea215d246fef50a1d021df44b8a9f68626
Brian Barton <brian.n.barton@gmail.com>
2021-01-27 12:17:18 -0700

    troubleshooting displaying the board

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -6,8 +6,8 @@
 
 ######
 
-class GameState():
-    def _init_(self):
+class GameState:
+    def __init__(self):
         # this board is an 8x8 two dimentional list.
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
@@ -19,4 +19,4 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
         self.whiteToMove = True
-        self.moveLog = [True]
+        self.moveLog = []
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -4,11 +4,13 @@
 
 
 import pygame as p
+from chess.svg import board
+
 from Chess import ChessEngine
 
 WIDTH = HEIGHT = 512  # optional 400
-DIMENSIONS = 8  # dimensions are 8x8
-SQ_SIZE = HEIGHT // DIMENSIONS
+DIMENSION = 8  # dimensions are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15  # For animation
 IMAGES = {}
 
@@ -17,11 +19,11 @@ Initialize global dictionary of images. this will be called once in the main
 '''
 
 
-def load_images():
+def loadImages():
     pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR", "bp", "bR", "bN", "bB", "bQ", "bK", "bB", "bN",
               "bR"]
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + "wp.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load("Chess/Chess/images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
 
 #         note: we can access an image by saying 'IMAGES['wp']
@@ -37,16 +39,15 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(gs.board)
-    load_images()  # only operated once
+    loadImages()  # only operated once
     running = True
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-            drawGameState((screen, gs))
-            clock.tick(MAX_FPS)
-            p.display.flip()
+        drawGameState((screen, gs))
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
 
 def drawGameState(screen, gs):
@@ -55,17 +56,24 @@ def drawGameState(screen, gs):
 
 
 def drawBoard(screen):
-    colors = [p.Color("white", p.Color("grey"))]
-    for r in range(DIMENSIONS):
-        for c in range(DIMENSIONS):
-            color = colors[((r + c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    colors = [p.Color("white", p.Color("green"))]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
 
 
 def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
     '''
     draws the pieces on the board using current gameState.board
     '''
+
 
 
 if __name__ == "__main__":
